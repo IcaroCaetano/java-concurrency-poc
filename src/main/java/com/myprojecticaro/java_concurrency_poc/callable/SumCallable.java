aa84317b014c74799b29e4556f095d0e3cf437d5
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-21 23:29:27 -0300

    Update SumCallable.java

--- src/main/java/com/myprojecticaro/java_concurrency_poc/callable/SumCallable.java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/callable/SumCallable.java
@@ -1,1 +1,18 @@
 
+import java.util.concurrent.Callable;
+
+public class SumCallable implements Callable<Integer> {
+
+    private final int a;
+    private final int b;
+
+    public SumCallable(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public Integer call() {
+        return a + b;
+    }
+}
