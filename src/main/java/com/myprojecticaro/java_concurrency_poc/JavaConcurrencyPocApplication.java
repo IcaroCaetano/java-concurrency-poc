5e32d8434288479166601b3a05b2a484ac18f411
IcaroCaetano <icaro.caetano@gmail.com>
2026-01-21 23:24:46 -0300

    first commit

---
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/JavaConcurrencyPocApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.java_concurrency_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrencyPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConcurrencyPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/java_concurrency_poc/JavaConcurrencyPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.java_concurrency_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrencyPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
