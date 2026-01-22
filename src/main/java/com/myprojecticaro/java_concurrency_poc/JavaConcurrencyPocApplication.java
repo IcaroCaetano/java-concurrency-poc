633f0949f8e468e9cc8b1835c0d87d9be97ed287
IcaroCaetano <icaro.caetano@gmail.com>
2026-01-22 09:03:26 -0300

    feat: update main test

--- src/main/java/com/myprojecticaro/java_concurrency_poc/JavaConcurrencyPocApplication.java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/JavaConcurrencyPocApplication.java
@@ -1,6 +1,8 @@
 package com.myprojecticaro.java_concurrency_poc;
 
+import com.myprojecticaro.java_concurrency_poc.callable.DelayedCallable;
 import com.myprojecticaro.java_concurrency_poc.callable.SimpleMessageCallable;
+import com.myprojecticaro.java_concurrency_poc.callable.SumCallable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -19,12 +21,35 @@ public class JavaConcurrencyPocApplication {
 
 			/*
 			 * - SimpleMessageCallable
+			 * , teste simples de callable
 			 */
 			Future<String> messageFuture =
 					executor.submit(new SimpleMessageCallable());
 
 			System.out.println("SimpleMessageCallable result:");
 			System.out.println(messageFuture.get());
+			System.out.println();
+
+			/*
+			 * 2. SumCallable
+			 * , Callable com estado
+			 */
+			Future<Integer> sumFuture =
+					executor.submit(new SumCallable(10, 20));
+
+			System.out.println("SumCallable result:");
+			System.out.println(sumFuture.get());
+			System.out.println();
+
+			/*
+			 * 3. DelayedCallable
+			 * Simula processamento demorado
+			 */
+			Future<String> delayedFuture =
+					executor.submit(new DelayedCallable(1000));
+
+			System.out.println("DelayedCallable result:");
+			System.out.println(delayedFuture.get());
 			System.out.println();
 
 		} catch (InterruptedException e) {
