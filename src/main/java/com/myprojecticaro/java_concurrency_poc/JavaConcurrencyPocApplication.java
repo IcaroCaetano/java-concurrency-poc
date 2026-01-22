698cb9983f0a1a78b59202e1b540f1fca5a51710
IcaroCaetano <icaro.caetano@gmail.com>
2026-01-22 08:57:27 -0300

    feat: add test callable

--- src/main/java/com/myprojecticaro/java_concurrency_poc/JavaConcurrencyPocApplication.java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/JavaConcurrencyPocApplication.java
@@ -3,11 +3,17 @@ package com.myprojecticaro.java_concurrency_poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootApplication
 public class JavaConcurrencyPocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaConcurrencyPocApplication.class, args);
+
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+
+
 	}
 
 }
