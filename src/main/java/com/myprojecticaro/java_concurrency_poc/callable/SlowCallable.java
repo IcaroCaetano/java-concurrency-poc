6d0e41c719b3da77d7696e19316ec4651c7e2835
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-01-27 23:34:17 -0300

    Update SlowCallable.java

--- src/main/java/com/myprojecticaro/java_concurrency_poc/callable/SlowCallable.java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/callable/SlowCallable.java
@@ -1,1 +1,13 @@
+package com.example.concurrency.callable;
+
+import java.util.concurrent.Callable;
+
+public class SlowCallable implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(2000);
+        return "Finished";
+    }
+}
 
