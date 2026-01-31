c95bac5b83e777707b87d5124a063bc265348942
IcaroCaetano <icaro.caetano@gmail.com>
2026-01-30 22:50:52 -0300

    feat: run ExecutorShutdownExample

---
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/executor/ExecutorShutdownExample.java
@@ -0,0 +1,36 @@
+package com.myprojecticaro.java_concurrency_poc.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorShutdownExample {
+
+    public static void run() {
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        executor.execute(() -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("Task finished");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        executor.shutdown();
+
+        try {
+            // Bloqueia até que todas as tarefas concluam a execução após uma solicitação
+            // de desligamento, ou até que o tempo limite seja atingido,
+            if (!executor.awaitTermination(3, TimeUnit.SECONDS)) {
+                System.out.println("Forcing shutdown");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+}
