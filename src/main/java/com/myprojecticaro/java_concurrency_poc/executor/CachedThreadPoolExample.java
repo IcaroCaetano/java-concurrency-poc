96488464aa49dbac7df0ae79d10f9aad546f4886
IcaroCaetano <icaro.caetano@gmail.com>
2026-01-30 22:43:38 -0300

    feat: aff new cached trhead

---
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/executor/CachedThreadPoolExample.java
@@ -0,0 +1,27 @@
+package com.myprojecticaro.java_concurrency_poc.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPoolExample {
+
+    public static void run() {
+
+        // Cria um pool de threads que cria novas threads conforme necessário
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 1; i <= 10; i++) {
+            int taskId = i;
+
+            executor.execute(() -> {
+                System.out.println(
+                        "Task " + taskId +
+                        " executed by " +
+                        Thread.currentThread().getName()
+                );
+            });
+        }
+
+        executor.shutdown();
+    }
+}
