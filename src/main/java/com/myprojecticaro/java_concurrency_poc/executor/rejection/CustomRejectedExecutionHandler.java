26ac7f3f885fe1d1c5f6509876e36b2a06b87bd5
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-02-03 23:14:21 -0300

    Update CustomRejectedExecutionHandler.java

--- src/main/java/com/myprojecticaro/java_concurrency_poc/executor/rejection/CustomRejectedExecutionHandler.java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/executor/rejection/CustomRejectedExecutionHandler.java
@@ -1,1 +1,18 @@
+package com.myprojecticaro.java_concurrency_poc.executor.rejection;
+
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class CustomRejectedExecutionHandler implements RejectedExecutionHandler {
+
+    @Override
+    public void rejectedExecution(Runnable task, ThreadPoolExecutor executor) {
+        System.out.println(
+            "Task rejected | Active: " +
+            executor.getActiveCount() +
+            " | Queue size: " +
+            executor.getQueue().size()
+        );
+    }
+}
 
