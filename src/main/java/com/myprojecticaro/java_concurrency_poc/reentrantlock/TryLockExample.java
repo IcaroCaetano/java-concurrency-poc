4ce9c8d8170340391e51d3e0ecb0b2e400560972
IcaroCaetano <icaro.caetano@gmail.com>
2026-02-07 20:49:35 -0300

    feat: add ReentrantLock

---
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/reentrantlock/TryLockExample.java
@@ -0,0 +1,36 @@
+package com.myprojecticaro.java_concurrency_poc.reentrantlock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TryLockExample {
+
+    public static void run() {
+        ReentrantLock lock = new ReentrantLock();
+
+        Runnable task = () -> {
+            if (lock.tryLock()) {
+                try {
+                    System.out.println(Thread.currentThread().getName() +
+                            " acquired the lock");
+                    sleep(500);
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println(Thread.currentThread().getName() +
+                        " could not acquire the lock");
+            }
+        };
+
+        new Thread(task, "Thread-A").start();
+        new Thread(task, "Thread-B").start();
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
