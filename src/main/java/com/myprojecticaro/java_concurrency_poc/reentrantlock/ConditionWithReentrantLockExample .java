59f7707cc23a206dbeb247d603792dc374581248
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-02-10 23:34:43 -0300

    Update ConditionWithReentrantLockExample .java

--- src/main/java/com/myprojecticaro/java_concurrency_poc/reentrantlock/ConditionWithReentrantLockExample .java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/reentrantlock/ConditionWithReentrantLockExample .java
@@ -1,1 +1,16 @@
+package java-concurrency-poc.src.main.java.com.myprojecticaro.java_concurrency_poc.reentrantlock;
+
+public class ConditionWithReentrantLockExample {
+
+    public static void run() {
+
+        ConditionBuffer buffer = new ConditionBuffer(3);
+
+        Thread producer = new Thread(new ConditionProducer(buffer), "Producer");
+        Thread consumer = new Thread(new ConditionConsumer(buffer), "Consumer");
+
+        producer.start();
+        consumer.start();
+    }
+}
 
