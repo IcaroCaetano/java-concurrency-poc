8b0437be2d87652ecdfced37566dab1779f32121
IcaroCaetano <icaro.caetano@gmail.com>
2026-02-11 10:00:40 -0300

    feat: add AtomicLongAccumulatorExample

--- src/main/java/com/myprojecticaro/java_concurrency_poc/atomic/AtomicLongAccumulatorExample.java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/atomic/AtomicLongAccumulatorExample.java
@@ -4,6 +4,17 @@ import java.util.concurrent.atomic.LongAdder;
 
 public class AtomicLongAccumulatorExample {
 
+    /**
+     * Uma ou mais variáveis que, juntas, mantêm uma soma longa inicialmente igual a zero.
+     * Quando as atualizações (método add) são disputadas entre threads, o conjunto de variáveis
+     * pode crescer dinamicamente para reduzir a disputa. O método sum (ou, equivalentemente, longValue) retorna o
+     * total atual combinado entre as variáveis ​​que mantêm a soma.
+     *
+     * Essa classe geralmente é preferível a AtomicLong quando várias threads atualizam uma soma comum que é
+     * usada para fins como coleta de estatísticas, e não para controle de sincronização refinado. Sob baixa disputa
+     * de atualização, as duas classes têm características semelhantes. Mas sob alta disputa, a taxa de transferência
+     * esperada dessa classe é significativamente maior, à custa de maior consumo de espaço.
+     */
     private static final LongAdder adder = new LongAdder();
 
     public static void run() {
