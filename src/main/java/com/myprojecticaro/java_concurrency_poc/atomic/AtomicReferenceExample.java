bfed0436116087bc39a2963c156b98557f1d68f4
IcaroCaetano <icaro.caetano@gmail.com>
2026-02-11 09:56:31 -0300

    feat: add AtomicReferenceExample

--- src/main/java/com/myprojecticaro/java_concurrency_poc/atomic/AtomicReferenceExample.java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/atomic/AtomicReferenceExample.java
@@ -6,8 +6,19 @@ public class AtomicReferenceExample {
 
     public static void run() {
 
+        /**
+         *Uma referência de objeto que pode ser atualizada atomicamente. Consulte a especificação VarHandle para
+         * obter descrições das propriedades dos acessos atômicos.
+         */
         AtomicReference<String> reference = new AtomicReference<>("Initial");
 
+
+        /**
+         * Atualiza atomicamente (com os efeitos de memória especificados por VarHandle.compareAndSet) o valor atual
+         * com os resultados da aplicação da função fornecida, retornando o valor atualizado. A função deve ser livre
+         * de efeitos colaterais, visto que pode ser reaplicada quando as tentativas de atualização falharem devido à
+         * contenção entre threads.
+         */
         reference.updateAndGet(current -> current + " -> Updated");
 
         System.out.println("Value: " + reference.get());
