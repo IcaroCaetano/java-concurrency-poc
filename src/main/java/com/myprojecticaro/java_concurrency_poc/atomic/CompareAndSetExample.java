c6a81c515147e95a42a7a9bb5218102fc5f3d017
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-02-11 09:37:24 -0300

    Update CompareAndSetExample.java

--- src/main/java/com/myprojecticaro/java_concurrency_poc/atomic/CompareAndSetExample.java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/atomic/CompareAndSetExample.java
@@ -1,1 +1,17 @@
+package com.myprojecticaro.java_concurrency_poc.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CompareAndSetExample {
+
+    public static void run() {
+
+        AtomicInteger value = new AtomicInteger(10);
+
+        boolean updated = value.compareAndSet(10, 20);
+
+        System.out.println("Updated: " + updated);
+        System.out.println("Current value: " + value.get());
+    }
+}
 
