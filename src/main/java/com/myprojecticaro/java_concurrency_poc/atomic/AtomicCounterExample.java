ceea3b08ff4739c1b5b9f7703e00f2685eff63fc
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-02-11 09:32:17 -0300

    Update AtomicCounterExample.java

--- src/main/java/com/myprojecticaro/java_concurrency_poc/atomic/AtomicCounterExample.java
+++ src/main/java/com/myprojecticaro/java_concurrency_poc/atomic/AtomicCounterExample.java
@@ -1,1 +1,39 @@
+package com.myprojecticaro.java_concurrency_poc.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicCounterExample {
+
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
+    public static void run() {
+
+        Runnable task = () -> {
+            // 5 tarefas
+            for (int i = 0; i < 5; i++) {
+                int value = counter.incrementAndGet();
+                System.out.println(Thread.currentThread().getName() + " incremented to " + value);
+            }
+        };
+
+        Thread t1 = new Thread(task, "Thread-1");
+        Thread t2 = new Thread(task, "Thread-2");
+
+        t1.start();
+        t2.start();
+
+        join(t1, t2);
+    }
+
+    private static void join(Thread... threads) {
+        for (Thread t : threads) {
+            try {
+               // 
+                t.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
 
